2a48f120cea72ea0d5e5b43de46c9bee33ab0045
amrit <amrit@MSI>
2022-06-16 15:54:33 +0530

    done

---
+++ day9/src/day9/Demo8.java
@@ -0,0 +1,23 @@
+package day9;
+
+public class Demo8 {
+
+	public static void main(String[] args) {
+		for(int i = 0; i<= 2; i++) {
+
+			for (int j = i; j<=5; j++) {
+
+			if(j == 4) { //4==4
+
+			continue; //forecful iteration
+
+			}
+
+			System.out.println(j); //01235 1235 235
+
+			}
+
+	}
+
+	}
+}
